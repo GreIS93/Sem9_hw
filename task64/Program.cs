21cef0bd2645648689fc97dd4154b5b267d0530a
GreIS93 <grekovis93@gmail.com>
2022-07-24 12:55:41 +0700

    task68

---
+++ task64/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа кратные 3-ём в промежутке от M до N.
+// M = 1; N = 9. -> "3, 6, 9" M = 13; N = 20. -> "15, 18"
+
+void MultipleOfThree(int m, int n)
+{
+    if (m > n) return;
+    if (m % 3 == 0) Console.Write($"{m}, ");
+    MultipleOfThree(m + 1, n);
+}
+
+Console.WriteLine("Введите число M");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+MultipleOfThree(m, n);
+
---
+++ task68/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Ackermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Ackermann(m - 1, 1);
+    else if (m > 0 && n > 0) return Ackermann(m - 1, Ackermann(m, n - 1));
+    else return 0;
+}
+
+Console.WriteLine("Введите число M");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(Ackermann(m, n));
